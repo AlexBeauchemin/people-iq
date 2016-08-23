5cc93a01e048aeb8f22f379d3e0965c00426d177
Alex Beauchemin <abeauchemin@sweetiq.com>
2016-08-23 10:32:54 -0400

    nvmrc use browser sync with rollup

---
+++ browsersync.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  files: [
+    'index.html',
+    'dist'
+  ],
+  server: './',
+  port: 3000,
+  open: false,
+  notify: true,
+  injectChanges: true
+};
--- src/containers/page-home.jsx
+++ src/containers/page-home.jsx
@@ -4,6 +4,7 @@ class PageHome extends React.Component {
   render() {
     return (
       <main>
+        <h2>Hey!</h2>
         Hello
       </main>
     );
